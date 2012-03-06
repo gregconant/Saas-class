1d13fb20e0ecb160fef444c9f3deaffc2879b317
Greg Conant <greg.conant@gmail.com>
2012-03-06 07:09:22 -0500

    gc - adding basic code for HW2 Part 1.  no work done yet.

---
+++ hw2-part1.rb
@@ -0,0 +1,38 @@
+#HW 2: More ruby, and some rails
+
+#1. open classes, metaprogramming, duck typing
+#a) [ELLS ex. 3.11] Extend the currency-conversion example from lecture so that you can write
+#5.dollars.in(:euros)
+#10.euros.in(:rupees)
+#etc.
+#● You should support the currencies 'dollars', 'euros', 'rupees' , 'yen' where the
+#conversions are: rupees to dollars, multiply by 0.019; yen to dollars, multiply by 0.013;
+#euro to dollars, multiply by 1.292.
+#● Both the singular and plural forms of each currency should be acceptable, e.g.
+#1.dollar.in(:rupees) and 10.rupees.in(:euro) should work.
+#You can use the code shown in lecture as a starting point if you wish; 
+# it is at http://pastebin.com/#agjb5qBF
+#b) Adapt your solution from HW 1 "palindromes" question so that instead of writing palindrome?
+#("foo") you can write "foo".palindrome? HINT: this should require fewer than 5 lines of code.
+#c) Adapt your palindrome solution so that it works on Enumerables. That is:
+#[1,2,3,2,1].palindrome? # => true
+#(It's not necessary for the collection's elements to be palindromes themselves--only that the
+#top-level collection be a palindrome.) HINT: this should require fewer than 5 lines of code.
+#Although hashes are considered Enumerables, your solution does not need to make sense for
+#hashes (though it should not error)
+
+# metaprogramming to the rescue!
+
+
+# from pastebin link above
+class Numeric
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  def method_missing(method_id)
+    singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self * @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+end
